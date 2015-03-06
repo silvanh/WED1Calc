065120ae99f9db1945f33d46612c7f0998ead76a
silvanh <shabegge@hsr.ch>
2015-03-06 13:17:09 +0100

    first try

--- js/calc.js
+++ js/calc.js
@@ -1,3 +1,17 @@
 /**
  * Created by silvan on 3/6/15.
  */
+window.addEventListener("load", function(){
+    var numberFields = document.querySelectorAll(".numberFields *");
+    for(var i=0;i<numberFields.length;i++) {
+        var currentValue = numberFields[i].value;
+        numberFields[i].addEventListener('click',setValueOfHeader(numberFields[i].value));
+    }
+});
+
+
+function setValueOfHeader(x){
+    console.log("clicked with value = "+x);
+    var headField = document.querySelector(".header *");
+    headField.value=x;
+}
