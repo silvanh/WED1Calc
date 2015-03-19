53e091d5da028d2e935dce49d108fcb6c83ecfd9
silvanh <shabegge@hsr.ch>
2015-03-19 09:35:41 +0100

    added function to recalculate with a result

--- js/calc.js
+++ js/calc.js
@@ -7,8 +7,11 @@ $(function() {
     $(".numberFields > input").on('click', function(){
         var number = $(this).attr('name');
         $('#outputField').attr('value', function(i, origValue) {
-            if(origValue === operator || isResult || origValue === '0' ){
+            if(origValue === operator || origValue === '0' ){
+                return number;
+            }else if(isResult){
                 isResult=false;
+                operator='';
                 return number;
             }
             return origValue + number;
@@ -19,6 +22,7 @@ $(function() {
         operator = $(this).attr('name');
         $('#outputField').attr('value', function(i, origValue) {
             operand1 = origValue;
+            isResult=false;
             return operator;
         });
     });
