720240b9464668421642188cc11e376fafb8fb21
silvanh <shabegge@hsr.ch>
2015-03-19 09:24:25 +0100

    editet calc function, its now possible to calc more than one time

--- js/calc.js
+++ js/calc.js
@@ -2,11 +2,13 @@ $(function() {
     var operand1;
     var operand2;
     var operator = '';
+    var isResult = false;
 
     $(".numberFields > input").on('click', function(){
         var number = $(this).attr('name');
         $('#outputField').attr('value', function(i, origValue) {
-            if(origValue === operator || origValue === '0'){
+            if(origValue === operator || isResult || origValue === '0' ){
+                isResult=false;
                 return number;
             }
             return origValue + number;
@@ -23,15 +25,25 @@ $(function() {
 
     $('#sumButton').on('click', function() {
         $('#outputField').attr('value', function(i, origValue) {
-            operand2 = origValue;
+            if(!isResult) {
+                operand2 = origValue;
+            }
             if(operator === '+'){
-                return parseInt(operand1) + parseInt(operand2);
+                operand1=parseInt(operand1) + parseInt(operand2);
+                isResult=true;
+                return operand1;
             } else if(operator === '-'){
-                return parseInt(operand1) - parseInt(operand2);
+                operand1=parseInt(operand1) - parseInt(operand2);
+                isResult=true;
+                return operand1;;
             } else if(operator === '*'){
-                return parseInt(operand1) * parseInt(operand2);
-            } else if(operator === '/'){    
-                return parseInt(operand1) / parseInt(operand2);
+                operand1=parseInt(operand1) * parseInt(operand2);
+                isResult=true;
+                return operand1;
+            } else if(operator === '/'){
+                operand1=parseInt(operand1) / parseInt(operand2);
+                isResult=true;
+                return operand1;
             }else {
                 return origValue;
             }
