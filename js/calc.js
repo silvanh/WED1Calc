620e872c2fabbd2fb7f14932bb327ba97dec5340
silvanh <shabegge@hsr.ch>
2015-03-20 11:07:18 +0100

    correctet js with jslint

--- js/calc.js
+++ js/calc.js
@@ -1,56 +1,60 @@
-$(function() {
-    var operand1;
-    var operand2;
-    var operator = '';
-    var isResult = false;
+$(function () {
+    "use strict";
+    var operand1 = 0,
+        operand2 = 0,
+        operator = '',
+        isResult = false;
 
-    $(".numberFields > input").on('click', function(){
+    $(".numberFields > input").on('click', function () {
         var number = $(this).attr('name');
-        $('#outputField').attr('value', function(i, origValue) {
-            if(origValue === operator || origValue === '0' ){
+        $('#outputField').attr('value', function (i, origValue) {
+            if (origValue === operator || origValue === '0') {
                 return number;
-            }else if(isResult){
-                isResult=false;
-                operator='';
+            }
+            if (isResult) {
+                isResult = false;
+                operator = '';
                 return number;
             }
             return origValue + number;
         });
     });
 
-    $(".operations > input").on('click',function(){
+    $(".operations > input").on('click', function () {
         operator = $(this).attr('name');
-        $('#outputField').attr('value', function(i, origValue) {
+        $('#outputField').attr('value', function (i, origValue) {
             operand1 = origValue;
-            isResult=false;
+            isResult = false;
             return operator;
         });
     });
 
-    $('#sumButton').on('click', function() {
-        $('#outputField').attr('value', function(i, origValue) {
-            if(!isResult) {
+    $('#sumButton').on('click', function () {
+        $('#outputField').attr('value', function (i, origValue) {
+            if (!isResult) {
                 operand2 = origValue;
             }
-            if(operator === '+'){
-                operand1=parseInt(operand1) + parseInt(operand2);
-                isResult=true;
+            if (operator === '+') {
+                operand1 = parseInt(operand1, 10) + parseInt(operand2, 10);
+                isResult = true;
                 return operand1;
-            } else if(operator === '-'){
-                operand1=parseInt(operand1) - parseInt(operand2);
-                isResult=true;
-                return operand1;;
-            } else if(operator === '*'){
-                operand1=parseInt(operand1) * parseInt(operand2);
-                isResult=true;
-                return operand1;
-            } else if(operator === '/'){
-                operand1=parseInt(operand1) / parseInt(operand2);
-                isResult=true;
-                return operand1;
-            }else {
-                return origValue;
             }
+            if (operator === '-') {
+                operand1 = parseInt(operand1, 10) - parseInt(operand2, 10);
+                isResult = true;
+                return operand1;
+            }
+            if (operator === '*') {
+                operand1 = parseInt(operand1, 10) * parseInt(operand2, 10);
+                isResult = true;
+                return operand1;
+            }
+            if (operator === '/') {
+                operand1 = parseInt(operand1, 10) / parseInt(operand2, 10);
+                isResult = true;
+                return operand1;
+            }
+            return origValue;
         });
     });
 });
