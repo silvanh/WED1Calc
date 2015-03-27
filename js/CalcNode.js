1bf7d8c9a4815cd5ea256344d74ef674f9514269
silvan <silvan@linux-a6oy.site>
2015-03-27 11:40:49 +0100

    added node.js server and deadLink

---
+++ js/CalcNode.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+http.createServer(function (req, res) {
+    'use strict'
+    var url = req.url;
+    if (url === "/index.html" || url === "/calc.html") {
+        sendFile(res, "../index.html", "text/html");
+    } else if (url === "/css/testat1-Design2.css") {
+        sendFile(res, "../css/testat1-Design2.css", "text/css");
+    } else if (url === "/js/calc.js") {
+        sendFile(res, "../js/calc.js", "text/javascript");
+    }else if (url === "/deadLink.png") {
+        sendFile(res, "../pictures/deadLink.png", "image/gif");
+    } else{
+        sendFile(res, "../deadLink.html", "text/html");
+    }
+}).listen(3000);
+
+console.log('Server running at http://localhost:3000/');
+
+function sendFile(res, fileName, contentType) {
+    fs.readFile(fileName, function (err, data) {
+        if (err) {
+            throw err;
+        }
+        res.writeHeader(200, {"Content-Type": contentType});
+        res.write(data);
+        res.end();
+    });
+}
